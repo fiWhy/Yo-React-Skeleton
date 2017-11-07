bdf75c58a8671e56fbc33f9591f7cb263b2c41c8
Денис <denis.buryachkovsky@gmail.com>
2017-11-07 22:34:24 +0200

    Fixed issues with not renamed folders in templates

--- generators/app/templates/src/app.tsx
+++ generators/app/templates/src/app.tsx
@@ -1,7 +1,7 @@
 ﻿import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import App from "./components/app";
+import App from "./containers/app";
 import Router from "./config/routes";
 import AppStore from "./store";
 
